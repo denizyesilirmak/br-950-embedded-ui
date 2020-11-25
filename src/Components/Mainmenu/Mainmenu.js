55aee73a9d8bbb22d8c3866d157acb12724a95fd
denizyesilirmak <denizyesilirmak@gmail.com>
2020-11-25 11:15:49 +0300

    initial commit

---
+++ src/Components/Mainmenu/Mainmenu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Mainmenu.css'
+
+class Mainmenu extends React.Component{
+  render(){
+    return(
+      <div className="main-menu">
+        
+      </div>
+    )
+  }
+}
+
+export default Mainmenu
