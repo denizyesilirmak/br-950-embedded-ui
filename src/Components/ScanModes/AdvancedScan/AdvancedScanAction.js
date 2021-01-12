0787da06578813bcf13868842044bdb4bec94909
denizyesilirmak <denizyesilirmak@gmail.com>
2021-01-12 16:33:49 +0300

    added: advanced scan video time ranges to send line commands

--- src/Components/ScanModes/AdvancedScan/AdvancedScanAction.js
+++ src/Components/ScanModes/AdvancedScan/AdvancedScanAction.js
@@ -5,16 +5,52 @@ import ScanVideo from '../../../Assets/videos/automatic-scan-video.mp4'
 
 
 class AdvancedScanAction extends React.Component {
+  constructor(props) {
+    super(props)
+    this.scanVideoRef = React.createRef()
+
+  }
   onVideoEnded = () => {
     // console.log('video ended')
     this.props.navigateTo('scanViewerAdvancedScreen', { test: 'test' })
+  }
+
+  componentDidMount() {
+    //FIXME: update timespamps with exact ones.
+    this.interval = setInterval(() => {
+      switch (Math.trunc(this.scanVideoRef.current.currentTime)) {
+        case 3:
+          console.log('A')
+          break
+        case 6:
+          console.log('B')
+          break
+        case 9:
+          console.log('C')
+          break
+        case 12:
+          console.log('D')
+          break
+        case 15:
+          console.log('E')
+          break
+        case 18:
+          console.log('F')
+        case 21:
+          console.log('Z')
+          clearInterval(this.interval)
+          break
+        default:
+          break;
+      }
+    }, 1000);
   }
 
 
   render() {
     return (
       <div className="advanced-scan-action">
-        <video src={ScanVideo} autoPlay muted onEnded={this.onVideoEnded} />
+        <video src={ScanVideo} autoPlay muted onEnded={this.onVideoEnded} ref={this.scanVideoRef} />
       </div>
     )
   }
